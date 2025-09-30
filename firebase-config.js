933815a6c9eaa5ed9c8b723f8da8505c80324f3a
ElisaPre <elisa.pregaldiny@gmail.com>
2025-09-30 13:23:55 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,17 @@
-// firebase-config.js
+// firebase-config.js (version compatible GitHub Pages)
 
+// Configuration Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyBSHfjUjc9nTaZW2NwMC8PxeQgJNWpxVio",
   authDomain: "seve-plant-hunt.firebaseapp.com",
   projectId: "seve-plant-hunt",
-  storageBucket: "seve-plant-hunt.appspot.com",
+  storageBucket: "seve-plant-hunt.appspot.com", // correction
   messagingSenderId: "791467451009",
   appId: "1:791467451009:web:9f92aae2784f62cb6f6380",
   measurementId: "G-0383K60P1K"
 };
 
-// Initialize Firebase
+// Initialisation Firebase
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
