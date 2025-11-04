395e1da7f184e4f4ca0aa03545bff44384cc91eb
ElisaPre <elisa.pregaldiny@gmail.com>
2025-11-04 17:02:59 +0100

    Fix page link for 'Plante 2' in cards.js

--- js/cards.js
+++ js/cards.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // Liste des plantes avec pages associées
   const cards = [
     { name: "Plante 1", page: "plantes/agave.html", image: "Agave.png" },
-    { name: "Plante 2", page: "plantes/erable.html", image: "Arbre de Judée.png" },
+    { name: "Plante 2", page: "plantes/Arbre-de-Judee.html", image: "Arbre de Judée.png" },
     { name: "Plante 3", page: "plantes/pin.html", image: "Cyprès vert.png" },
     { name: "Plante 4", page: "plantes/plante4.html", image: "Faux poivrier.png" },
     { name: "Plante 5", page: "plantes/plante5.html", image: "Figuier de barbarie.png" },
