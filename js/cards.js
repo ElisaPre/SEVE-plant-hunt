b16c9a8f94c811028eca3458b7c5f10d03e5204a
ElisaPre <elisa.pregaldiny@gmail.com>
2025-11-05 09:38:26 +0100

    Fix page link for Plante 12 in cards.js

--- js/cards.js
+++ js/cards.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", () => {
     { name: "Plante 9", page: "plantes/Magnolia.html", image: "Magnolia.png" },
     { name: "Plante 10", page: "plantes/Olivier.html", image: "Olivier.png" },
     { name: "Plante 11", page: "plantes/Pin parasol.html", image: "Pin parasol.png" },
-    { name: "Plante 12", page: "plantes/plante5.html", image: "Sumac de Virginie.png" },
+    { name: "Plante 12", page: "plantes/Sumac de Virginie.html", image: "Sumac de Virginie.png" },
     { name: "Plante 13", page: "plantes/plante5.html", image: "Vigne.png" },
     { name: "Plante 14", page: "plantes/plante5.html", image: "Yucca.png" }
   ];
